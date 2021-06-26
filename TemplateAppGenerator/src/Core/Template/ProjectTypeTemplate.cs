20a249af7e897d3b27bf6e638e0e0b9494a7f293
Egliss <egoparadise.rest@gmail.com>
2021-06-26 17:13:36 +0900

    Add Templates and FastEnum

---
+++ TemplateAppGenerator/src/Core/Template/ProjectTypeTemplate.cs
@@ -0,0 +1,54 @@
+using FastEnumUtility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateAppGenerator.Core.Template
+{
+
+    public enum ProjectType
+    {
+        Console,
+        ASP,
+    }
+
+    public sealed class ProjectTypeTemplate : IProjectTemplateContent
+    {
+        public string name => "project-type";
+
+        public ProjectType type { get; private set; } = ProjectType.Console;
+
+        public void InteractUser(in TemplateArguments argument)
+        {
+            var console = argument.processor;
+            var blank = new string(' ', argument.hierarchy * 2);
+            this.type = console.WaitSelectInput(new SelectInputRequest<ProjectType>()
+            {
+                items = FastEnum.GetValues<ProjectType>(),
+                defaultValue = ProjectType.Console,
+                text = $"{blank}choose project type"
+            });
+
+            this.Dispatch(argument);
+        }
+
+        private void Dispatch(in TemplateArguments argument)
+        {
+            var arg = argument with { hierarchy = argument.hierarchy + 1 };
+            switch (this.type)
+            {
+                case ProjectType.Console:
+                    break;
+                case ProjectType.ASP:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Generate(in TemplateArguments argument)
+        {
+
+        }
+    }
+}
